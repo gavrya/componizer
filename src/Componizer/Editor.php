b9e5706a1833d074a9d4e6865c64ec02bc893878
Gavrischuk Alexander <gavrya@gmail.com>
2015-10-15 21:11:16 +0300

    First commit

---
+++ src/Componizer/Editor.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gavrya
+ * Date: 10/15/15
+ * Time: 9:08 PM
+ */
+
+namespace Componizer;
+
+
+class Editor
+{
+
+    public static function run()
+    {
+        die('It works');
+    }
+
+}
