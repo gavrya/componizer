4c2f50c1f03e873de94cdd9c8cd3f1e8a7d0008d
Gavrischuk Alexander <gavrya@gmail.com>
2015-11-28 18:50:17 +0200

    Implemented external/internal js assets

--- src/Skeleton/ComponizerExternalJs.php
+++ src/Skeleton/ComponizerExternalJs.php
@@ -9,9 +9,82 @@
 namespace Gavrya\Componizer\Skeleton;
 
 
+use InvalidArgumentException;
+
 class ComponizerExternalJs
 {
 
-    // TODO: ComponizerJsAsset with __toString() = <script type="text/javascript" src="/componizer/component-id/js/file.js" defer></script>
+    // include positions
+    const POSITION_TOP = 'top';
+    const POSITION_BOTTOM = 'bottom';
 
+    // execution modes
+    const MODE_DEFAULT = 'default';
+    const MODE_ASYNC = 'async';
+    const MODE_DEFER = 'defer';
+
+    // internal vars
+    private $path = null;
+    private $position = null;
+    private $mode = null;
+
+    //-----------------------------------------------------
+    // Create/init section
+    //-----------------------------------------------------
+
+    public function __construct($path, $position = self::POSITION_BOTTOM, $mode = self::MODE_DEFAULT)
+    {
+        // check js src path
+        if ($path === null || !is_string($path) || strtolower(substr($path, -strlen('.js'))) !== '.js') {
+            throw new InvalidArgumentException('Invalid path');
+        }
+
+        $this->path = $path;
+
+        // check js include position
+        if (!in_array($position, [self::POSITION_TOP, self::POSITION_BOTTOM])) {
+            throw new InvalidArgumentException('Invalid position');
+        }
+
+        $this->position = $position;
+
+        // check js execution mode
+        if (!in_array($mode, [self::MODE_DEFAULT, self::MODE_ASYNC, self::MODE_DEFER])) {
+            throw new InvalidArgumentException('Invalid mode');
+        }
+
+        $this->mode = $mode;
+    }
+
+    //-----------------------------------------------------
+    // General methods section
+    //-----------------------------------------------------
+
+    public function path()
+    {
+        return $this->path;
+    }
+
+    public function position()
+    {
+        return $this->position;
+    }
+
+    public function mode()
+    {
+        return $this->mode;
+    }
+
+    //-----------------------------------------------------
+    //  Magic methods section
+    //-----------------------------------------------------
+
+    public function __toString()
+    {
+        if ($this->mode === self::MODE_DEFAULT) {
+            return '<script type="text/javascript" src="' . $this->path . '"></script>';
+        } else {
+            return '<script type="text/javascript" src="' . $this->path . '" ' . $this->mode . '></script>';
+        }
+    }
 }
--- src/Skeleton/ComponizerInternalJs.php
+++ src/Skeleton/ComponizerInternalJs.php
@@ -12,4 +12,60 @@ namespace Gavrya\Componizer\Skeleton;
 class ComponizerInternalJs
 {
 
+    // include positions
+    const POSITION_TOP = 'top';
+    const POSITION_BOTTOM = 'bottom';
+
+    // internal vars
+    private $script = null;
+
+    //-----------------------------------------------------
+    // Create/init section
+    //-----------------------------------------------------
+
+    public function __construct($script, $position = self::POSITION_BOTTOM)
+    {
+        // check js src path
+        if (
+            $script === null ||
+            !is_string($script) ||
+            strtolower(substr(trim($script), 0, strlen('<script>'))) !== '<script>' ||
+            strtolower(substr(trim($script), -strlen('</script>'))) !== '</script>'
+        ) {
+            throw new InvalidArgumentException('Invalid script');
+        }
+
+        $this->script = $script;
+
+        // check js include position
+        if (!in_array($position, [self::POSITION_TOP, self::POSITION_BOTTOM])) {
+            throw new InvalidArgumentException('Invalid position');
+        }
+
+        $this->position = $position;
+    }
+
+    //-----------------------------------------------------
+    // General methods section
+    //-----------------------------------------------------
+
+    public function script()
+    {
+        return $this->script;
+    }
+
+    public function position()
+    {
+        return $this->position;
+    }
+
+    //-----------------------------------------------------
+    //  Magic methods section
+    //-----------------------------------------------------
+
+    public function __toString()
+    {
+        return $this->script;
+    }
+
 }
